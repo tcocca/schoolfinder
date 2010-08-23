f225439f577784e0bc8ecc3cc6e22f8e1136ff9e
tcocca <tom.cocca@gmail.com>
2010-08-23 09:46:39 -0400

    cleaned up the spec, let rspec describe the spec

--- spec/schoolfinder/client_spec.rb
+++ spec/schoolfinder/client_spec.rb
@@ -10,9 +10,7 @@ describe "Schoolfinder::Client" do
       mock_get({"f" => "schoolSearch", "zip" => "29601"}, 'school_search.json')
     end
     
-    it "should not return an error" do
-      lambda {@response = @schoolfinder.school_search(:zip => "29601")}.should_not raise_exception
-    end
+    it { lambda {@response = @schoolfinder.school_search(:zip => "29601")}.should_not raise_exception }
   end
   
   context "test_rating" do
@@ -20,9 +18,7 @@ describe "Schoolfinder::Client" do
       mock_get({"f" => "getTestRating", "nces_id" => "061029001146"}, 'test_rating.json')
     end
     
-    it "should not return an error" do
-      lambda {@response = @schoolfinder.test_rating(:nces_id => "061029001146")}.should_not raise_exception
-    end
+    it { lambda {@response = @schoolfinder.test_rating(:nces_id => "061029001146")}.should_not raise_exception }
   end
   
   context "student_diversity" do
@@ -30,9 +26,7 @@ describe "Schoolfinder::Client" do
       mock_get({"f" => "getStudentDiversity", "nces_id" => "450231000564"}, 'student_diversity.json')
     end
     
-    it "should not return an error" do
-      lambda {@response = @schoolfinder.student_diversity(:nces_id => "450231000564")}.should_not raise_exception
-    end
+    it { lambda {@response = @schoolfinder.student_diversity(:nces_id => "450231000564")}.should_not raise_exception }
   end
   
   context "test_scores" do
@@ -40,9 +34,7 @@ describe "Schoolfinder::Client" do
       mock_get({"f" => "getTestScores", "nces_id" => "450231000564"}, 'test_scores.json')
     end
     
-    it "should not return an error" do
-      lambda {@response = @schoolfinder.test_scores(:nces_id => "450231000564")}.should_not raise_exception
-    end
+    it { lambda {@response = @schoolfinder.test_scores(:nces_id => "450231000564")}.should_not raise_exception }
   end
   
   context "teacher_stats" do
@@ -50,9 +42,7 @@ describe "Schoolfinder::Client" do
       mock_get({"f" => "getTeacherStats", "nces_id" => "450231000564", "districtleaid" => "4502310"}, 'teacher_stats.json')
     end
     
-    it "should not return an error" do
-      lambda {@response = @schoolfinder.teacher_stats(:nces_id => "450231000564", :districtleaid => "4502310")}.should_not raise_exception
-    end
+    it { lambda {@response = @schoolfinder.teacher_stats(:nces_id => "450231000564", :districtleaid => "4502310")}.should_not raise_exception }
   end
   
   context "student_stats" do
@@ -60,9 +50,7 @@ describe "Schoolfinder::Client" do
       mock_get({"f" => "getStudentStats", "nces_id" => "450231000564", "districtleaid" => "4502310", "city" => "Greenville", "state" => "SC"}, 'student_stats.json')
     end
     
-    it "should not return an error" do
-      lambda {@response = @schoolfinder.student_stats(:nces_id => "450231000564", :districtleaid => "4502310", :city => "Greenville", :state => "SC")}.should_not raise_exception
-    end
+    it { lambda {@response = @schoolfinder.student_stats(:nces_id => "450231000564", :districtleaid => "4502310", :city => "Greenville", :state => "SC")}.should_not raise_exception }
   end
   
   context "reviews" do
@@ -70,9 +58,7 @@ describe "Schoolfinder::Client" do
       mock_get({"f" => "getReviews", "schoolid" => "25381"}, 'reviews.json')
     end
     
-    it "should not return an error" do
-      lambda {@response = @schoolfinder.reviews(:schoolid => "25381")}.should_not raise_exception
-    end
+    it { lambda {@response = @schoolfinder.reviews(:schoolid => "25381")}.should_not raise_exception }
   end
   
   context "number_of" do
@@ -80,9 +66,7 @@ describe "Schoolfinder::Client" do
       mock_get({"f" => "numberOf", "city" => "Greenville", "state" => "SC"}, 'number_of.json')
     end
     
-    it "should not return an error" do
-      lambda {@response = @schoolfinder.number_of("Greenville", "SC")}.should_not raise_exception
-    end
+    it { lambda {@response = @schoolfinder.number_of("Greenville", "SC")}.should_not raise_exception }
   end
   
   context "district_search" do
@@ -90,9 +74,7 @@ describe "Schoolfinder::Client" do
       mock_get({"f" => "districtSearch", "city" => "Greenville", "state" => "SC"}, 'district_search.json')
     end
     
-    it "should not return an error" do
-      lambda {@response = @schoolfinder.district_search(:city => "Greenville", :state => "SC")}.should_not raise_exception
-    end
+    it { lambda {@response = @schoolfinder.district_search(:city => "Greenville", :state => "SC")}.should_not raise_exception }
   end
   
   context "branding_data" do
@@ -100,9 +82,7 @@ describe "Schoolfinder::Client" do
       mock_get({"f" => "gbd", "nces_id" => "450231000564"}, 'branding_data.json')
     end
     
-    it "should not return an error" do
-      lambda {@response = @schoolfinder.branding_data(:nces_id => "450231000564")}.should_not raise_exception
-    end
+    it { lambda {@response = @schoolfinder.branding_data(:nces_id => "450231000564")}.should_not raise_exception }
   end
   
 end
